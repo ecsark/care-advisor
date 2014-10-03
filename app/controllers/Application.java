428a78663d572f12d34b8c9212e8db910c51c846
Edward Cheng <edward.cheng@hotmail.com>
2014-10-03 21:36:33 +0800

    add json exchange example

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,5 +1,9 @@
 package controllers;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import play.libs.Json;
+import play.mvc.BodyParser;
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.index;
@@ -7,7 +11,24 @@ import views.html.index;
 public class Application extends Controller {
 
     public static Result index() {
-        return ok(index.render("Care for you."));
+        return ok(index.render("Care for you~"));
     }
 
+    @BodyParser.Of(BodyParser.Json.class)
+    public static Result jsonHello() {
+        JsonNode json = request().body().asJson();
+        if (json==null) {
+            return badRequest("Expecting Json data");
+        }
+        String name = json.findPath("name").textValue();
+        if (name==null) {
+            return badRequest("Missing parameter [name]");
+        }
+
+        ObjectNode result = Json.newObject();
+        result.put("greeting","Hello "+name);
+        return ok(result);
+
+    }
 }
+
